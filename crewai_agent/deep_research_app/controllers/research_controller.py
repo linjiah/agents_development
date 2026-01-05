b756127e33050937d86247486465293d3bead2f1
linjia <linjiah@mtu.edu>
2026-01-04 16:57:16 -0800

    deep research agent

---
+++ crewai_agent/deep_research_app/controllers/research_controller.py
@@ -0,0 +1,9 @@
+from services.agents_service import setup_agents_and_tasks
+from models.pdf_generator import create_pdf
+from utils.markdown_cleaner import clean_markdown
+import base64
+
+extracted_links = []
+
+# Task 7: Implements run_deep_research function
+
